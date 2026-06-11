70f807bbbba5c9f7e554dff28d809afd034ec357
Samiksha Moghan <samikshamoghan@gmail.com>
2026-06-11 13:25:36 +0800

    added Search bar

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,12 @@
 import Heading from "../components/Heading";
+import SearchBar from "../components/SearchBar";
+
 function Dashboard() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-950 transition-colors duration-200">
-      <div className="max-w-2xl mx-auto px-4 py-10">
-        <Heading title="CSV Collaboration" style="mb-4"/>
+      <div className="mx-auto px-4 py-10">
+        <Heading title="CSV Collaboration" style="mb-4 text-center"/>
+        <SearchBar />
       </div>
     </div>
   );
